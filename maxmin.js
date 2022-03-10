d697234ae70f958cf3a4eee9f2789fec3ca0eb99
Dan Michell <dan.michell1000@gmail.com>
2022-03-10 18:35:59 +0000

    Completed max and min challenge

--- maxmin.js
+++ maxmin.js
@@ -1,1 +1,16 @@
-//Use nested for loops
+function maxMin(arr) {
+    let maxVal = arr[0]
+    let minVal = arr[0]
+
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > maxVal) {
+            maxVal = arr[i]
+        } else if (arr[i] < minVal) {
+            minVal = arr[i]
+        }
+    }
+
+    return [maxVal,minVal]
+}
+
+console.log(maxMin([20, 50, 12, 6, 14, 8]))
